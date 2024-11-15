75f878c66dd344e085f61dd64b567fdab39a924c
HEX9CF <hex9cf@aliyun.com>
2024-11-15 11:43:35 +0800

    Add fitness_function

---
+++ fitness_function.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+from objective_function import objective_function
+
+# 解码
+def decode(binary, bounds, gene_size):
+    decimal = int("".join(map(str, binary)), 2)
+    return bounds[0] + (bounds[1] - bounds[0]) * decimal / (2**gene_size - 1)
+
+# 适应值函数
+def fitness_function(population, bounds, gene_size):
+    fitness = []
+    for i in population :
+        x1_binary = i[:gene_size]
+        x2_binary = i[gene_size:]
+
+        x1 = decode(x1_binary, bounds, gene_size)
+        x2 = decode(x2_binary, bounds, gene_size)
+
+        fitness_value = objective_function([x1, x2])
+        fitness.append(fitness_value)
+    return np.array(fitness)
+
+
--- objective_function.py
+++ objective_function.py
@@ -1,5 +1,5 @@
 import numpy as np
 
 # 目标函数
-def objective_function(x, j):
-    return 3 - np.sin(j * x[0])**2 - np.sin(j * x[1])**2
+def objective_function(x):
+    return 3 - np.sin(2 * x[0])**2 - np.sin(2 * x[1])**2
