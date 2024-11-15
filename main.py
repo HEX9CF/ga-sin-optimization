a45af531128e4f1a2fed255881c58d37ad3c5ee7
HEX9CF <hex9cf@aliyun.com>
2024-11-15 11:55:16 +0800

    Add gene_count

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+from function.fitness_function import fitness_function
+from population import init_population
+
+bounds = [0, 6]
+population_size = 1000
+gene_size = 23
+gene_count = 2
+
 def main():
-    print("Hello World!")
+    population = init_population(population_size, gene_size, gene_count)
+    print("初始种群：")
+    print(population)
+
+    fitness = fitness_function(population, bounds, gene_size)
+    print("适应值：")
+    print(fitness)
+
+
+
+
 
 if __name__ == "__main__":
     main()
